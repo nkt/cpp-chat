baa4adb23ebfbbe9c3cf123183d1fe7e5d4c1cdb
Gusakov Nikita <dev@nkt.me>
2014-04-05 14:07:20 +0400

    Initial Commit

---
+++ server/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  server
+//
+//  Created by Gusakov Nikita on 05/04/14.
+//  Copyright (c) 2014 nkt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
